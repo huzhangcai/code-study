e69e25513897409248bd3b795b2fff0905247043
jiangtonghu <jiangtonghu@sunline.cn>
2022-04-01 20:35:50 +0800

    add java toolchain to gradle build file

--- question-service/build.gradle.kts
+++ question-service/build.gradle.kts
@@ -8,7 +8,11 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
-
+java{
+    toolchain{
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
+}
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.8.2"))
     testImplementation("org.junit.jupiter:junit-jupiter-api")
